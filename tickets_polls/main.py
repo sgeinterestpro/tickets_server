e54662428f4f862aa0c6c8ece51cf90030a75344
muumlover <muumlover@live.com>
2019-04-24 18:06:19 +0800

    1、增加 配置文件 2、增加 票券对象 3、增加 数据库连接 4、增加 票券列表接口 5、增加 领取票券接口

--- tickets_polls/main.py
+++ tickets_polls/main.py
@@ -1,24 +1,12 @@
-import pathlib
-
-import yaml
 from aiohttp import web
 
+from config import setup_config
+from database import setup_database
 from routes import setup_routes
 
-
-def load_config(config_path):
-    with open(config_path, 'r') as f:
-        cfg = f.read()
-        print(type(cfg))  # 读出来是字符串
-        print(cfg)
-        config = yaml.load(cfg, Loader=yaml.BaseLoader)  # 用load方法转字典
-    return config
-
-
 app = web.Application()
-conf = load_config(str(pathlib.Path('.') / 'config' / 'polls.yaml'))
-app['config'] = conf
+setup_config(app)
 setup_routes(app)
-host, port = conf['server']['host'], int(conf['server']['port'])
-
+setup_database(app)
+host, port = app['config']['server']['host'], int(app['config']['server']['port'])
 web.run_app(app, host=host, port=port)
--- tickets_polls/routes.py
+++ tickets_polls/routes.py
@@ -3,7 +3,9 @@ from views import *
 
 def setup_routes(app):
     app.router.add_route('*', '/', default_handle)
-    app.router.add_route('*', '/ticket/{ticket_id}', Ticket.ticket)
-    app.router.add_get('/ticket_apply', Ticket.apply)
-    app.router.add_post('/ticket_used', Ticket.used)
-    app.router.add_post('/ticket_delete', Ticket.delete)
+    app.router.add_route('*', '/ticket', TicketRouter)
+    app.router.add_route('*', '/ticket/{ticket_id}', TicketRouter)
+    # app.router.add_get('/ticket_history', Ticket.history)
+    # app.router.add_get('/ticket_apply', Ticket.apply)
+    # app.router.add_post('/ticket_used', Ticket.used)
+    # app.router.add_post('/ticket_delete', Ticket.delete)
