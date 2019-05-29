4b7802572855ad847d80452fa4074237f5de8b07
muumlover <muumlover@live.com>
2019-05-29 17:23:43 +0800

    [增加] 增加票券过期处理任务 1、增加 定时任务处理过期票券 2、修改 票券核验时更多返回消息

--- tickets_polls/main.py
+++ tickets_polls/main.py
@@ -4,16 +4,23 @@ datetime: 2019-04-19
 author: muumlover
 """
 
+import logging
+
 from aiohttp import web
 
 from config import setup_config
 from database import setup_database
 from routes import setup_routes, setup_middleware
+from task import setup_task
+
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=logging.INFO)
 
 app = web.Application()
 setup_config(app)
 setup_routes(app)
-setup_database(app)
 setup_middleware(app)
+setup_database(app)
+setup_task(app)  # 需要在数据库设置完成后设置
 host, port = app['config']['server']['host'], int(app['config']['server']['port'])
 web.run_app(app, host=host, port=port)
---
+++ tickets_polls/tasks/expiry.py
@@ -0,0 +1,22 @@
+"""
+filename: expiry.py
+datetime: 2019-05-29
+author: muumlover
+"""
+import logging
+from datetime import datetime
+
+
+async def expiry(app):
+    db = app['db']
+    date_now = datetime.now().strftime('%Y-%m-%d')
+    res = await db.ticket.update_one({
+        'state': 'valid',
+        'expiry_date': {'$lt': date_now}
+    }, {
+        '$set': {
+            'state': 'expired',
+            'overdue_time': datetime.now()
+        }
+    })
+    logging.info(f'本次处理过期票券{res.modified_count}张')
