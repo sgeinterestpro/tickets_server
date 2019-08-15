48973a99eeed254963440b500e52aa77239d9200
SamWang <muumlover@live.com>
2019-08-15 12:50:23 +0800

    rsa 框架

--- tickets_polls/config.py
+++ tickets_polls/config.py
@@ -7,6 +7,8 @@ author: muumlover
 import pathlib
 
 import yaml
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 def load_config(config_path):
@@ -19,3 +21,13 @@ def load_config(config_path):
 def setup_config(app):
     conf = load_config(str(pathlib.Path('.') / 'config' / 'polls.yaml'))
     app['config'] = conf
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    app['private_key'] = private_key
+    app['public_key'] = public_key
---
+++ tickets_polls/views/v_system.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@Time    : 2019/8/13 21:34 
+@Author  : Sam
+@Email   : muumlover@live.com
+@Blog    : https://blog.muumlover.com
+@Project : tickets_server
+@FileName: v_system
+@Software: PyCharm
+@license : (C) Copyright 2019 by muumlover. All rights reserved.
+@Desc    : 
+    
+"""
+from aiohttp import web
+from cryptography.hazmat.primitives import serialization
+
+
+class SystemHandles:
+    @staticmethod
+    async def rsa_pub_key(request):
+        public_pem = request.app['public_key'].public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return web.Response(text=public_pem.decode())
