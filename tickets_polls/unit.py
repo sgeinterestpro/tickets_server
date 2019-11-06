4da13516ff6f173e348f642f32b183075c36faf0
muumlover <muumlover@live.com>
2019-11-06 14:00:41 +0800

    一种更准确的日期计算方法

--- tickets_polls/unit.py
+++ tickets_polls/unit.py
@@ -38,7 +38,13 @@ def date_week_start(date=None):
     """
     if date is None:
         date = datetime.now()
-    return date - timedelta(days=date.weekday())
+    return date - timedelta(
+        days=date.weekday(),
+        hours=date.hour,
+        minutes=date.minute,
+        seconds=date.second,
+        microseconds=date.microsecond
+    )
 
 
 def date_week_end(date=None):
@@ -48,12 +54,19 @@ def date_week_end(date=None):
     """
     if date is None:
         date = datetime.now()
-    return date + timedelta(days=6 - date.weekday())
+    return date + timedelta(
+        days=6 - date.weekday(),
+        hours=23 - date.hour,
+        minutes=59 - date.minute,
+        seconds=59 - date.second,
+        microseconds=-date.microsecond
+    )
 
 
 def date_month_start(date=None):
     """
     给定日期所在结算周的所属月开始的第一天
+    某周日所在自然月即认定为某周所在的自然月
     :return:
     """
     if date is None:
@@ -72,5 +85,18 @@ def date_month_end(date=None):
     """
     if date is None:
         date = datetime.now()
-    next_month = datetime(year=date.year, month=date.month + 1, day=date.day)
-    return date_month_start(next_month) + timedelta(days=-1)
+    date = date_week_end(date)
+    if date.month < 12:
+        next_month = datetime(year=date.year, month=date.month + 1, day=15)
+    else:
+        next_month = datetime(year=date.year + 1, month=1, day=15)
+    return date_month_start(next_month) + timedelta(days=-1, hours=23, minutes=59, seconds=59)
+
+
+if __name__ == '__main__':
+    date_test = None
+    date_test = datetime(year=2019, month=12, day=20)
+    print(date_week_start(date_test))
+    print(date_week_end(date_test))
+    print(date_month_start(date_test))
+    print(date_month_end(date_test))
