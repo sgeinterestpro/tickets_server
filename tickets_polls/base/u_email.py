3d12ba3f886e346377b654a7068191f18cd9ba8c
muumlover <muumlover@live.com>
2019-12-03 17:31:35 +0800

    增加邮件代理发送方式

--- tickets_polls/base/u_email.py
+++ tickets_polls/base/u_email.py
@@ -20,6 +20,7 @@ def setup_email(app: Application) -> None:
         raise Exception('需要初始化配置参数')
     EmailSender.charset = app['config']['email']['charset']
     EmailSender.sender = app['config']['email']['sender']
+    EmailSender.servers = app['config']['email']['servers']
     app['email'] = EmailSender
 
 
@@ -45,12 +46,9 @@ class EmailSender:
     sender = None
 
     @staticmethod
-    async def send(to_addrs, subject, mail_msg, attachs=None):
-        logging.debug(('邮件收件人：', to_addrs))
+    async def _make_email(to_addrs, subject, mail_msg, attachs):
         logging.debug((to_addrs, subject, mail_msg, attachs))
-
         charset = EmailSender.charset
-        from_addr = EmailSender.sender
         if isinstance(to_addrs, str):
             to_addrs = [to_addrs]
 
@@ -76,27 +74,29 @@ class EmailSender:
                 message.attach(attachment)
 
         message['Subject'] = Header(subject, charset=charset).encode()
-        message['From'] = f'{rfc2047("票券管理平台", charset)} <{from_addr}>'
+        message['From'] = f'{rfc2047("票券管理平台", charset)} <{EmailSender.sender}>'
         message['To'] = ';'.join([f'{rfc2047("平台用户", charset)} <{to_addr}>' for to_addr in to_addrs])
 
-        # with open('send_email.eml', 'wb') as fd:
-        #     logging.debug('邮件已经保存到本地文件')
-        #     fd.write(message.as_bytes())
-        #     return
+    @staticmethod
+    async def _send_direct(message, to_addrs):
+        to_domain_set = set([to_addr.split('@')[1] for to_addr in to_addrs])
+        for to_domain in to_domain_set:
+            send_addrs = [x for x in to_addrs if to_domain in x]
+            try:
+                with SmtpDirectServer(to_domain) as smtp_server:
+                    send_errs = smtp_server.send_message(message, EmailSender.sender, send_addrs)
+                    if not send_errs:
+                        logging.debug(f'邮件投递到{to_domain}成功')
+                    else:
+                        logging.error(f'邮件投递到{to_domain}失败')
+                        logging.error(send_errs)
+            except dns.resolver.NoAnswer:
+                logging.error(f'服务器 {to_domain} MX 记录解析失败')
 
+    @staticmethod
+    async def _try_send_direct(message, to_addrs):
         try:
-            to_domain_set = set([to_addr.split('@')[1] for to_addr in to_addrs])
-            for to_domain in to_domain_set:
-                try:
-                    with SmtpServer(to_domain) as smtp_server:
-                        send_errs = smtp_server.send_message(message, from_addr, to_addrs)
-                        if not send_errs:
-                            logging.debug(f'邮件投递到{to_domain}成功')
-                        else:
-                            logging.error(f'邮件投递到{to_domain}失败')
-                            logging.error(send_errs)
-                except dns.resolver.NoAnswer:
-                    logging.error(f'服务器 {to_domain} MX 记录解析失败')
+            await EmailSender._send_direct(message, to_addrs)
         except smtplib.SMTPDataError as err:
             logging.error(f'邮件投递失败')
             logging.exception(err)
@@ -106,13 +106,63 @@ class EmailSender:
             logging.exception(err)
             raise smtplib.SMTPDataError(-1, b'Unknown Error')
 
+    @staticmethod
+    async def _try_send_proxy(message, to_addrs):
+        for server in EmailSender.servers:
+            try:
+                logging.debug('使用邮件服务器：' + server['host'])
+                with SmtpProxyServer(server['host'], 25, server['user'], server['pass']) as smtp_server:
+                    send_errs = smtp_server.sendmail(EmailSender.sender, to_addrs, message)
+                    if not send_errs:
+                        logging.debug('邮件发送成功')
+                        return
+                    else:
+                        logging.error('邮件发送失败')
+                        logging.error(send_errs)
+            except Exception as e:
+                logging.exception(e)
+        raise smtplib.SMTPDataError(-1, b'Unknown Error')
 
-class SmtpServer:
+    @staticmethod
+    async def send(to_addrs, subject, mail_msg, attachs=None):
+        logging.debug(('邮件收件人：', to_addrs))
+        message = await EmailSender._make_email(to_addrs, subject, mail_msg, attachs)
+        # with open('send_email.eml', 'wb') as fd:
+        #     logging.debug('邮件已经保存到本地文件')
+        #     fd.write(message.as_bytes())
+        #     return
+        try:
+            await EmailSender._try_send_direct(message, to_addrs)
+        except Exception as err:
+            logging.error(f'邮件直接投递失败')
+            logging.exception(err)
+            try:
+                await EmailSender._try_send_proxy(message, to_addrs)
+            except Exception as err:
+                logging.error(f'邮件中转投递失败')
+                logging.exception(err)
+                raise smtplib.SMTPDataError(-1, b'Unknown Error')
+
+
+class SmtpDirectServer:
     def __init__(self, domain):
         mx = dns.resolver.query(domain, 'MX')
         server_addr = mx[0].exchange.to_text()
         logging.debug(f'目标邮件服务器：{server_addr}')
         _server = smtplib.SMTP(server_addr)
+        self.server = _server
+
+    def __enter__(self):
+        return self.server
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.server.quit()
+
+
+class SmtpProxyServer:
+    def __init__(self, host, port, mail_user, mail_pass):
+        _server = smtplib.SMTP(host, port)
+        _server.login(mail_user, mail_pass)
         self.server = _server
 
     def __enter__(self):
